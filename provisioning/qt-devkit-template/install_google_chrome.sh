0701684ce89b0a27d43447ca2ecb0946724250b1
Aapo Keskimolo <aapo.keskimolo@qt.io>
2017-11-02 14:58:04 +0200

    Make provisioning file structure consistent with product repositories

--- provisioning/qt-devkit-template/install_google_chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# Install Coin development kit
-# Author: Aapo Keskimolo <aapo.keskimolo@qt.io>
-
-sudo apt install libgconf2-4 libnss3-1d libxss1
-dpkg -i /srv/pkg/google-chrome-stable_current_amd64.deb
--- provisioning/qt-devkit-template/prepare_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-sudo apt update
-sudo add-apt-repository universe
-

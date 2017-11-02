d090ce01462978264d98fd01837234d05c1dfe00
Aapo Keskimolo <aapo.keskimolo@qt.io>
2017-11-02 16:05:37 +0200

    Add bash_history to clean up operations

--- tools/clean.sh
+++ tools/clean.sh
@@ -3,6 +3,7 @@
 # Clean up user-specific information
 
 rm -f ~/.ssh/id_rsa* && rm -f ~/.ssh/config
+rm -f ~/.bash_history
 git config --global --unset-all user.name
 git config --global --unset-all user.email
 cp /srv/share/sync2vm/config ~/qt-devkit/sync2vm/config

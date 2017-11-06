65079d213bad3b9be39c650301206015b10a933e
Aapo Keskimolo <aapo.keskimolo@qt.io>
2017-11-06 15:37:41 +0200

    Add cifs-utils to install packages

--- coin/provisioning/qtci-linux-Ubuntu-16.04-x86_64-devkit/01-packages.sh
+++ coin/provisioning/qtci-linux-Ubuntu-16.04-x86_64-devkit/01-packages.sh
@@ -4,3 +4,5 @@
 sudo apt install -y nodejs nodejs-legacy npm \
             virtualenv python3-dev python3-pip python3-sphinx \
             cmake pkg-config flex bison tmux zip pigz p7zip-full
+
+sudo apt install -y cifs-utils

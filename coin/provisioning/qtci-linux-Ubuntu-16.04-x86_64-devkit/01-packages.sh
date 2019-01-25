918548765720daef171b0cd69160a17834bf439a
Aapo Keskimolo <Aapo Keskimolo>
2019-01-25 17:44:20 +0200

    Add python install in packages bundle

--- coin/provisioning/qtci-linux-Ubuntu-16.04-x86_64-devkit/01-packages.sh
+++ coin/provisioning/qtci-linux-Ubuntu-16.04-x86_64-devkit/01-packages.sh
@@ -11,6 +11,9 @@ sudo apt update
 sudo apt install -y nodejs npm  virtualenv python3-dev python3-pip \
             python-sphinx cmake pkg-config flex bison tmux zip pigz \
             p7zip-full kcachegrind openconnect
+python3 -m pip install --user pip setuptools
+echo "export PATH=\$PATH:~/.local/bin" >> ~/.bashrc
+python3 -m pip install --user pipenv
 
 # screen
 sudo apt install -y cifs-utils screen
